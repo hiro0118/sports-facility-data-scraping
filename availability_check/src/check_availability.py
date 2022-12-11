a20fcdd0a59b4f3ed7f27cbb6765d80a6bde89a1
hiro0118 <fuji.hiroshi0118@gmail.com>
2022-12-11 23:13:49 +0900

    Control scheduled mode by environment variable

--- availability_check/src/check_availability.py
+++ availability_check/src/check_availability.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 from time import sleep
 
+import schedule
 from availability import get_available_courts
 from notification import notify_availablity, notify_error
 from weather import get_weather
 
 INITAL_WAIT = 5
+LOOP_INTERVAL = 5
+
+SCHEDULE_TIMES = ["08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00",
+                  "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]
 
 
 def check_availability():
@@ -21,9 +26,13 @@ def check_availability():
         notify_error(e)
 
 
+def check_availability_scheduled():
+    for time in SCHEDULE_TIMES:
+        schedule.every().day.at(time).do(check_availability)
+    while True:
+        schedule.run_pending()
+        sleep(LOOP_INTERVAL)
+
+
 def get_current_datetime() -> str:
     return datetime.now().strftime("%Y-%m-%d %H:%M")
-
-
-if __name__ == "__main__":
-    check_availability()
---
+++ availability_check/src/main.py
@@ -0,0 +1,18 @@
+import os
+
+from check_availability import check_availability, check_availability_scheduled
+
+MODE_KEY = 'MODE'
+SCHEDULED_MODE = 'scheduled'
+
+
+def main():
+    mode = os.environ.get(MODE_KEY)
+    if mode == SCHEDULED_MODE:
+        check_availability_scheduled()
+    else:
+        check_availability()
+
+
+if __name__ == "__main__":
+    main()
